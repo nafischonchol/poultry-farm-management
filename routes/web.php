56d7e4f8601a2ee630622488f85e160011e1c7fa
Nafis Chonchol <nafisislam161@gmail.com>
2022-10-06 13:16:17 +0600

    install laravel

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\LoginRegController;
+
+// Route::get('store-reg-form',[LoginRegController::class,'regForm'])->name('store-reg-form');
+// Route::post('store-reg',[LoginRegController::class,'storeReg'])->name('store-reg');
+
+
+// Route::get('store-login-form',[LoginRegController::class,'loginForm'])->name('store-login-form');
+// Route::post('store-login',[LoginRegController::class,'storeLogin'])->name('store-login');
+// Route::get('store-logout',[LoginRegController::class,'storeLogout'])->name('store-logout');
