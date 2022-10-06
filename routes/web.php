cf51eb027ff6d425dc2504fb32ee80ea0943183c
Nafis Chonchol <nafisislam161@gmail.com>
2022-10-06 13:28:14 +0600

    install breeze

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-use App\Http\Controllers\LoginRegController;
-
-// Route::get('store-reg-form',[LoginRegController::class,'regForm'])->name('store-reg-form');
-// Route::post('store-reg',[LoginRegController::class,'storeReg'])->name('store-reg');
+use App\Http\Controllers\WarrientyController;
 
 
-// Route::get('store-login-form',[LoginRegController::class,'loginForm'])->name('store-login-form');
-// Route::post('store-login',[LoginRegController::class,'storeLogin'])->name('store-login');
-// Route::get('store-logout',[LoginRegController::class,'storeLogout'])->name('store-logout');
+Route::group(['middleware'=>['auth']],function(){
+    Route::get("/",[DashboardController::class,"index"])->name("dashboard");
+
+});
+
+require __DIR__.'/auth.php';
